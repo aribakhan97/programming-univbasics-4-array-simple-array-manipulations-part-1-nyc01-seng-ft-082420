d2a4c03dd18cbac473f8da518a5196520eafec18
Ariba Khan <akhan41@binghamton.edu>
2020-08-04 19:43:31 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop(array)
+end
+
+def using_pop_with_args(array)
+array.pop(2)
+end
+
+def using_shift(array)
+  array.shift(0)
+end
+
+def using_shift_with_args(array)
+  array.
